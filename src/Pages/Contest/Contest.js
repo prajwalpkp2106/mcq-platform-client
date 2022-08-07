7ea8b1cd7c5e671ec35dc61079cd28481758d897
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-08-07 23:31:46 +0530

    styling done

--- src/Pages/Contest/Contest.js
+++ src/Pages/Contest/Contest.js
@@ -3,69 +3,99 @@ import { Card, Button } from "antd";
 import { getContest } from "../../api/Requests";
 import Loader from "../../Components/Loader/Loader";
 import { connect } from "react-redux";
- const Data =[
+import { Link } from "react-router-dom";
+const Data = [
   {
-    name:"first"
-  }
- ]
-const Contest=()=> {
-    const [data,setData] = useState([]);
-    const [loading,setLoading] = useState(false);
-    // useEffect(()=>{
-    //     setLoading(true);
-    //     getContest()
-    //     .then((res)=>{
-    //         setData(res);
-    //         setLoading(false);
-    //     })
-    //     .catch((error)=>{})
+    name: "first",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/SVG_Logo.svg/1200px-SVG_Logo.svg.png",
+    _id: 12,
+  },
+  {
+    name: "first",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/SVG_Logo.svg/1200px-SVG_Logo.svg.png",
+  },
+  {
+    name: "first",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/SVG_Logo.svg/1200px-SVG_Logo.svg.png",
+  },
+  {
+    name: "first",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/SVG_Logo.svg/1200px-SVG_Logo.svg.png",
+  },
+];
+const Contest = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  // useEffect(()=>{
+  //     setLoading(true);
+  //     getContest()
+  //     .then((res)=>{
+  //         setData(res);
+  //         setLoading(false);
+  //     })
+  //     .catch((error)=>{})
 
-    // },[]);
+  // },[]);
 
-    
   return (
-    <>
-     {loading ? (
+    <div className="w-full md:w-10/12 mx-auto p-2 md:p-4 grid items-center md:grid-cols-3 gap-5 content-center">
+      {loading ? (
         <div>
           <Loader></Loader>
         </div>
       ) : (
         <>
-        {
-            Data.map((contest)=>{
-                return(
-                    <Card
-                    style={{
-                      margin: "30px",
-                      marginTop: "20px",
-                      border: "1px solid black",
-                      padding: "5px 5px",
-                    }}
-                  >
+          {Data.map((contest) => {
+            return (
+              <Card
+                style={{
+                  border: "1px solid black",
+                }}
+                className=" w-80 md:w-[400px] bg-black/20 inline-block text-white mx-auto"
+                title={
+                  <div className="text-white">
+                    <div className="w-full">
+                      <img
+                        className="h-[150px] mx-auto my-2"
+                        src={contest.image}
+                      ></img>
+                    </div>
+                    <div className=" font-bolds tracking-wider">Title</div>
+                  </div>
+                }
+              >
+                <div className="space-y-4">
+                  <div className="">
+                    Inner Card content Inner Card content Inner Card content
                     Inner Card content
-                    <Button type="primary" style={{ float: "right" }}>
-                      Primary Button
+                  </div>
+                  <div className="float-left">Starts In: 12:23:00</div>
+                  <Link to={`/contest/${contest._id}`}>
+                    <Button
+                      type="primary"
+                      className=" float-right border-white"
+                    >
+                      Detail
                     </Button>
-                  </Card>
-                
-                )
-            })
-        }
-       
-  
-       
-   </> 
-     )
-    }
-    </>
-  )
+                  </Link>
+                </div>
+              </Card>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
 
-}
-
-function mapStateToProps (state){
-    return {
-        isAuthenticated:state.isAuthenticated,
-    }
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: state.isAuthenticated,
+  };
 }
 
 export default connect(mapStateToProps)(Contest);
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,14 +3,18 @@ import "../Contest/Contest.css";
 
 export default function Home() {
   return (
-    <div className="h-[80%] bg-slate-200/10 text-center flex flex-col justify-center my-auto space-y-4">
-      <div>
-        <img
-          className=" w-80 md:w-[300px] mx-auto"
-          src="https://www.pictcsi.com/assets/CSI%20Logo%20Nav.png"
-        ></img>
+    <div className="h-full flex justify-center absolute top-0 left-0 text-center w-full p-4">
+      <div className="h-[80%] bg-slate-200/10 text-center flex flex-col backdrop-blur-sm justify-center w-full m-auto space-y-4 bg-gradient-to-b from-gray-400/20 to-gray-500/20">
+        <div>
+          <img
+            className=" w-80 md:w-[300px] mx-auto"
+            src="https://www.pictcsi.com/assets/CSI%20Logo%20Nav.png"
+          ></img>
+        </div>
+        <div className="text-6xl tracking-widest text-white font-extralight">
+          MCQ Platform
+        </div>
       </div>
-      <div className="text-6xl text-white">MCQ Platform</div>
     </div>
   );
 }
